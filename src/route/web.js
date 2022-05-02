38621fc0c4baf4b78c6c93de84ea776d42fd6a71
ndphong812 <ndphong812@gmail.com>
2022-05-02 18:21:01 +0700

    config mvc

---
+++ src/route/web.js
@@ -0,0 +1,12 @@
+import express from "express";
+import homeController from "../controller/homeController"
+let router = express.Router()
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomePage)
+    router.get('/about', (req, res) => {
+        res.send("This is About Page!")
+    })
+    return app.use("/", router)
+}
+export default initWebRoute;
--- src/server.js
+++ src/server.js
@@ -1,19 +1,12 @@
 const express = require('express')
 import configViewEngine from './configs/viewEngine'
+import initWebRoute from "./route/web"
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 8080;
 
 configViewEngine(app)
-app.get('/', (req, res) => {
-    // res.sendFile('./index.html', { root: __dirname })
-    res.render("index.ejs")
-})
-
-app.get('/about', (req, res) => {
-    res.send("This is About Page!")
-})
-
+initWebRoute(app)
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
